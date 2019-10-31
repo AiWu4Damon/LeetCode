11eada169cb49c262cfe509c04e6d89c979728a9
Damon <damon0425@outlook.com>
2019-10-31 09:17:36 +0800

    第二次提交 补齐代码

---
+++ restfulTemplate/src/main/java/com/damon/Solution5.java
@@ -0,0 +1,56 @@
+package com.damon;
+
+
+/**
+ * 给定两个大小为 m 和 n 的有序数组 nums1 和 nums2。
+
+ 请你找出这两个有序数组的中位数，并且要求算法的时间复杂度为 O(log(m + n))。
+
+ 你可以假设 nums1 和 nums2 不会同时为空。
+
+ 示例 1:
+
+ nums1 = [1, 3]
+ nums2 = [2]
+
+ 则中位数是 2.0
+ 示例 2:
+
+ nums1 = [1, 2]
+ nums2 = [3, 4]
+
+ 则中位数是 (2 + 3)/2 = 2.5
+ */
+class Solution5 {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int n =nums1.length;
+        int m =nums2.length;
+        //先在n上建立一个指针 默认使用nums1数组操作
+        int nIndex=(n-1)/2;
+//        int mIndex=(m+n)/2-
+        return 0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ restfulTemplate/src/main/java/com/damon/Solution6.java
@@ -0,0 +1,44 @@
+package com.damon;
+
+
+/**
+ * 反转一个单链表
+ 示例:
+
+ 输入: 1->2->3->4->5->NULL
+ 输出: 5->4->3->2->1->NULL
+ 进阶:
+ 你可以迭代或递归地反转链表。你能否用两种方法解决这道题？
+ */
+
+
+class Solution6 {
+    public static ListNode reverseList(ListNode head) {
+        if (head==null||head.next==null){return head;}
+        ListNode listNode = reverseList(head.next);
+        for (ListNode initNode=listNode;;){
+            if (initNode.next==null){
+                initNode.next=head;
+                head.next=null;
+                break;
+            }
+            initNode = initNode.next;
+        }
+        return listNode;
+    }
+
+    public static void main(String[] args) {
+        //1->2->3->4->5->NULL
+        ListNode listNode = new ListNode(1);
+        ListNode listNode1 = new ListNode(2);
+        ListNode listNode2 = new ListNode(3);
+        ListNode listNode3 = new ListNode(4);
+        ListNode listNode4 = new ListNode(5);
+        listNode.next=listNode1;
+        listNode1.next=listNode2;
+        listNode2.next=listNode3;
+        listNode3.next=listNode4;
+        ListNode listNode5 = reverseList(listNode);
+        System.out.println(listNode5);
+    }
+}
